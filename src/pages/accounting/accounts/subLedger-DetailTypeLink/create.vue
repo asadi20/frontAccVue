a626609d8b5063ab03d9f1f21f4dcf240a35508b
asadi20 <asadi20@gmail.com>
2026-04-19 22:26:29 +0330

    feat(accounting): add SubLedger–DetailType link creation form

---
+++ src/pages/accounting/accounts/subLedger-DetailTypeLink/create.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { linkDetailAccountType } from '@/services/subLedger-DetailAccounts';
+import { useRouter } from 'vue-router';
+
+const form = reactive({
+    account_id: '',
+    type_id: ''
+})
+const router = useRouter()
+const submit = async () => {
+    const res = await linkDetailAccountType(form);
+    alert(res);
+    if(res.success){
+        router.push('accounting-accounts-sub-ledger-detail-type-link')
+    }
+}
+</script>
+
+<template>
+    <VCard>
+        <VCardItem>
+            <VCardTitle>Add new link</VCardTitle>
+            <VCardSubtitle>links between account and detail account type</VCardSubtitle>
+        </VCardItem>
+        <VDivider />
+        <VCardText>
+            <Vform>
+                <VRow>
+                    <VCol>
+                        <VTextField label="subLedger account id" v-model="form.account_id" />
+                    </VCol>
+                    <VCol>
+                        <VTextField label="detail type_id" v-model="form.type_id" />
+                    </VCol>
+                </VRow>
+                <VRow>
+                    <VCol>
+                        <VBtn @click="submit">Submit </VBtn>
+                    </VCol>
+
+                </VRow>
+            </Vform>
+
+        </VCardText>
+    </VCard>
+</template>
