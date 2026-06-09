3bb4b25b5c5f94f444aa6928b89d6a36de7b007c
asadi20 <asadi20@gmail.com>
2026-06-09 16:17:42 +0330

    feat(api): implement auth service for user authentication and registration
    - add login method using Sanctum CSRF cookie flow
    - add register method
    - set appropriate headers for API requests (X-XSRF-TOKEN, Content-Type, etc.)
    - handle API base URLs and endpoints

---
+++ src/services/auth/authService.ts
@@ -0,0 +1,50 @@
+const API_BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'http://localhost:8000'
+
+// login method
+
+export const login = async (userData: any) => {
+    try {
+        await fetch(`${BASE_URL}/sanctum/csrf-cookie`, {
+            method: 'GET',
+            credentials: 'include'
+        })
+
+        const xsrfToken = document.cookie.split('; ').find(row => row.startsWith('XSRF-TOKEN='))?.split('=')[1]
+
+        const response = await fetch(`${API_BASE_URL}/auth/login`, {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-XSRF-TOKEN': xsrfToken ? decodeURIComponent(xsrfToken) : ''
+            },
+            credentials: 'include'
+        })
+
+        return await response.json()
+    } catch (error) {
+        throw error
+    }
+}
+
+// Register Method.
+export const register = async (userData: any) => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/auth/register`, {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const res = await response.json()
+        return res
+    } catch (error) {
+        throw error
+    }
+}
