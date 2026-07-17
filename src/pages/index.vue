148f8ad52d23d141579bcfb18d76815f005314fc
asadi20 <asadi20@gmail.com>
2026-07-17 12:45:46 +0330

    feat(dashboard): add stats cards and fetch dashboard metrics from API.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,21 +3,74 @@ meta:
   requiresAuth: true
 </route>
 
-<template>
-  <div>
-    <VCard class="mb-6" title="Kick start your project 🚀">
-      <VCardText>All the best for your new project.</VCardText>
-      <VCardText>
-        Please make sure to read our <a href="https://demos.pixinvent.com/vuexy-vuejs-admin-template/documentation/"
-          target="_blank" rel="noopener noreferrer" class="text-decoration-none">
-          Template Documentation
-        </a> to understand where to go from here and how to use our template.
-      </VCardText>
-    </VCard>
+<script setup lang="ts">
+const dash_stats = ref([
+  {
+    title: 'users',
+    stats: '3',
+    icon: 'tabler-users',
+    color: 'primary',
+  },
+  {
+    title: 'Documents',
+    stats: '120',
+    icon: 'tabler-file',
+    color: 'error'
+  },
+  {
+    title: 'Sales',
+    stats: '230M',
+    icon: 'tabler-basket-up',
+    color: 'success'
+  },
+  {
+    title: 'Buys',
+    stats: '220M',
+    icon: 'tabler-basket-down',
+    color: 'error'
+  },
+  {
+    title: 'Employee',
+    stats: '10',
+    icon: 'tabler-users-group',
+    color: 'primary'
+  }
+])
 
-    <VCard title="Want to integrate JWT? 🔒">
-      <VCardText>We carefully crafted JWT flow so you can implement JWT with ease and with minimum efforts.</VCardText>
-      <VCardText>Please read our JWT Documentation to get more out of JWT authentication.</VCardText>
-    </VCard>
-  </div>
+onMounted(async () => {
+  const response = await fetch(`/api/dash_stats`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+
+  const res = await response.json()
+  dash_stats.value = res.data
+})
+
+</script>
+
+<template>
+  <VCard title="Stats">
+    <VCardText>
+      <VRow>
+        <VCol cols="6" md="3" v-for="item in dash_stats" :key="item.title">
+          <div class="d-flex align-center gap-4 mt-md-9 mt-0">
+            <VAvatar :color="item.color" variant="tonal" rounded size="40">
+              <VIcon :icon="item.icon" />
+            </VAvatar>
+            <div class="d-flex flex-column">
+              <h5 class="text-h5">
+                {{ item.stats }}
+              </h5>
+              <div class="text-sm">
+                {{ item.title }}
+              </div>
+            </div>
+          </div>
+        </VCol>
+      </VRow>
+    </VCardText>
+  </VCard>
 </template>
