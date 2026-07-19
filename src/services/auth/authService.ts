fce587b39a0fa20f449bc7df1d310d04fa18fcd6
asadi20 <asadi20@gmail.com>
2026-07-19 15:47:17 +0330

    refactor(auth): support sanctum credentials and use env variables for api url

--- src/services/auth/authService.ts
+++ src/services/auth/authService.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:8000/api';
-const BASE_URL = 'http://localhost:8000'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL
 
 // login method
 
